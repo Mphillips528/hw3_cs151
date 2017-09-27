64422b9c45650e123a87011cc980b408762b56be
mattphillips <maphillips@loyola.edu>
2017-09-27 19:46:59 -0400

    Hw3 minus algorithm and test cases

---
+++ hw3.py
@@ -0,0 +1,15 @@
+name = input ("What is your name?")
+if name == "Raenita":
+    age = int (input("how old are you?"))
+    if age >= 30:
+        print ("You will be happily married.\n")
+    elif age >= 50:
+        print ("You will live a long life. \n")
+    else:
+        print ("You will win the lottery\n")
+else:
+    print ("Your name is unlucky\n")
+    print ("you will lose your winning lottery ticket\n")
+    print ("You will lose your job\n")
+    print ("You will get a divorse\n")
+    print ("You will be in a serious accident within the next 5 years\n")
