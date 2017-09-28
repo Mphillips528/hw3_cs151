e5a444cfb442c9f97cab30816a7e1935eed00f60
mattphillips <maphillips@loyola.edu>
2017-09-28 14:08:30 -0400

    Hw3

--- hw3.py
+++ hw3.py
@@ -1,9 +1,10 @@
+print("This is a program that will determine your fortune based on your name and other information. \n" )
 name = input ("What is your name?")
 if name == "Raenita":
     age = int (input("how old are you?"))
-    if age >= 30:
+    if age >= 30 and age <= 49:
         print ("You will be happily married.\n")
-    elif age >= 50:
+    elif age >= 50 and age <= 65 :
         print ("You will live a long life. \n")
     else:
         print ("You will win the lottery\n")
